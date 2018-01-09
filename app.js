b6ff09629215259b95f8936a00579abe12928454
Ruoyu Chen <ruoyu.chen@careeverywhere.com>
2018-01-09 08:59:11 -0500

    build initial master branch

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 new Vue({
     el: '#vue-app',
     data: {
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            {name: 'Ryu', age: 25},
-            {name: 'Yoshi', age: 35},
-            {name: 'Ken', age: 55}
-        ]
+
     },
     methods: {
     },
