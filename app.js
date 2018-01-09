dd216b619ed4540327410ed99c632b169a5f3e6a
Ruoyu Chen <ruoyu.chen@careeverywhere.com>
2018-01-09 11:23:24 -0500

    Components

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-new Vue({
-    el: '#vue-app',
-    data: {
-
+Vue.component('greeting', {
+    template: '<p>Hey there, I am a re-usable component named: {{name}}. <button v-on:click="changeName">Change Name</button></p>',
+    data: function() {
+        return {
+            name: 'Yoshi'
+        }
     },
     methods: {
-    },
-    computed: {
+        changeName: function() {
+            this.name = 'Mario';
+        }
     }
+});
+
+new Vue({
+    el: '#vue-app-one',
+});
+
+new Vue({
+    el: '#vue-app-two',
 });
