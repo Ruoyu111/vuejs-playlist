2389d84e822235807aafa60a8789cd2c4b3b18ab
Ruoyu Chen <ruoyu.chen@careeverywhere.com>
2018-01-22 10:53:27 -0500

    Global Components.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
       <h1>{{ title }}</h1>
-      <p>
-          {{ greeting() }}
-      </p>
+      <ninjas></ninjas>
   </div>
 </template>
 
@@ -11,12 +9,7 @@
 export default {
     data () {
         return {
-            title: 'Your first Vue file, wooo!'
-        }
-    },
-    methods: {
-        greeting: function() {
-            return 'heeeey cowboy!';
+            title: 'Ninja App'
         }
     }
 }
