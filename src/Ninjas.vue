19566529d662d064dcbe1f80603add6debbb3fb7
Ruoyu Chen <ruoyu.chen@careeverywhere.com>
2018-01-22 11:15:45 -0500

    Component CSS (scoped)

--- src/Ninjas.vue
+++ src/Ninjas.vue
@@ -1,9 +1,12 @@
 <template>
-    <ul>
-        <li v-for="ninja in ninjas">
-            {{ninja}}
-        </li>
-    </ul>
+    <div>
+        <h1>List of Ninjas</h1>
+        <ul>
+            <li v-for="ninja in ninjas">
+                {{ninja}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -16,6 +19,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+h1 {
+    color: green;
+}
 </style>
