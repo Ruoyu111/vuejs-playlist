4c9f8b7e39fd39d492d78e1362b399b7607c57cb
Ruoyu Chen <ruoyu.chen@careeverywhere.com>
2018-01-22 13:27:24 -0500

    Nested components.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <div>
-      <h1>{{ title }}</h1>
-      <ninjas></ninjas>
-  </div>
+    <div>
+        <app-header></app-header>
+        <app-content></app-content>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 
 export default {
     components: {
-        'ninjas': Ninjas
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-content': Ninjas
     },
-    data () {
+    data() {
         return {
-            title: 'Ninja App'
         }
     }
 }
 </script>
 
-<style scoped>
-h1 {
-    color: purple;
-}
+<style>
 </style>
