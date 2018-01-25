282cb2bb5174dd8b2dae623f0d1af97fe8e2a1b5
Ruoyu Chen <ruoyu.chen@careeverywhere.com>
2018-01-25 10:54:51 -0500

    Select box binding.

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -16,6 +16,10 @@
                 <label>Cheese</label>
                 <input type="checkbox" value="cheese" v-model="blog.categories" />
             </div>
+            <label>Author:</label>
+            <select v-model="blog.author">
+                <option v-for="a in authors">{{ a }}</option>
+            </select>
         </form>
         <div id="preview">
             <h3>Preview Blog</h3>
@@ -36,6 +40,9 @@
                     {{ c }}
                 </li>
             </ul>
+            <p>
+                Blog Author: {{ blog.author }}
+            </p>
         </div>
     </div>
 </template>
@@ -50,8 +57,10 @@ export default {
             blog: {
                 title: "",
                 content: "",
-                categories: []
-            }
+                categories: [],
+                author: ""
+            },
+            authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
         }
     },
     methods: {
